1854537624a0c62e90a081237706048e6ec84ed9
Victor Lucas Costa Viana <victor.viana@katrium.local>
2025-08-25 17:38:09 -0300

    feat: patentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,14 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 
+
 export const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
 	{ path: 'home', component: HomeComponent },
 	{ path: 'dashboard', component: DashboardComponent },
+	{
+		path: 'consultas',
+		loadComponent: () => import('./consultas/consultas.component').then(m => m.ConsultasComponent)
+	},
 	{ path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
