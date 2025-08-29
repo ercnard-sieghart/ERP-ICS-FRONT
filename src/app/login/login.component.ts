cec35791bc0df7752670be20f4959ba52faf9ea7
Victor Lucas Costa Viana <victor.viana@katrium.local>
2025-08-29 17:27:39 -0300

    feat: logica login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { PoDialogService } from '@po-ui/ng-components';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PoPageLoginModule, PoPageLogin } from '@po-ui/ng-templates';
@@ -14,12 +16,33 @@ import { PoPageLoginModule, PoPageLogin } from '@po-ui/ng-templates';
 export class LoginComponent {
   loading: boolean = false;
 
+  constructor(private http: HttpClient, private poDialog: PoDialogService) {}
+
   loginSubmit(loginData: any) {
     this.loading = true;
-    // loginData.login, loginData.password, loginData.rememberUser
-    setTimeout(() => {
-      this.loading = false;
-      // Adicione aqui a lógica de autenticação
-    }, 1500);
+    const payload = {
+      username: loginData.login,
+      password: loginData.password
+    };
+    this.http.post('http://localhost:8181/rest/login', payload, { observe: 'response' }).subscribe({
+      next: (response) => {
+        setTimeout(() => {
+          this.loading = false;
+          this.poDialog.alert({
+            title: 'Login realizado',
+            message: 'Autenticação realizada com sucesso!',
+          });
+        }, 1200);
+      },
+      error: (error: HttpErrorResponse) => {
+        setTimeout(() => {
+          this.loading = false;
+          this.poDialog.alert({
+            title: 'Falha no login',
+            message: 'Usuário ou senha inválidos.',
+          });
+        }, 1200);
+      }
+    });
   }
 }
