ca7bbfeace1f64e1503d620a0cc53ed03c5b15b0
vianadevelop <flood.com@hotmail.com>
2025-09-04 12:09:44 -0300

    Revert "feat: language-block"
    This reverts commit 71cb6bb189b92cda5ef7fe52292501a3c9924752.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 import {
   PoMenuItem,
@@ -8,36 +7,24 @@ import {
   PoPageModule,
   PoToolbarModule,
 } from '@po-ui/ng-components';
-
-import { LanguageBlockComponent, LanguageGuardService } from './language-block.component';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [
     CommonModule,
     PoToolbarModule,
     PoMenuModule,
     PoPageModule,
-    RouterOutlet,
-    LanguageBlockComponent // 🔹 importa o componente do bloqueio
-  ],
+    RouterOutlet
+],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   readonly menus: Array<PoMenuItem> = [
     { label: 'Home', action: this.onClick.bind(this) },
   ];
-
-  blocked = false; // 🔹 flag para controlar o bloqueio
-
-  constructor(private langGuard: LanguageGuardService) {}
-
-  ngOnInit(): void {
-    // 🔹 Verifica o idioma ao iniciar o app
-    this.blocked = this.langGuard.isBlocked();
-  }
 
   private onClick() {
     alert('Clicked in menu item');
