cf0062e658cb5166027957031ef25b7cf2146c50
vianadevelop <flood.com@hotmail.com>
2025-10-21 06:13:47 -0300

    refact: update dependencies and post config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://institutoclima128986.protheus.cloudtotvs.com.br:4050',
+  apiUrl: 'https://institutoclima128986.protheus.cloudtotvs.com.br:4050',
   apiBaseUrl: '/rest',
   appName: 'ERP ICS - Produção',
   version: '1.0.0'
