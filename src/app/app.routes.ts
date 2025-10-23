c899d8ce8ee5c30496325054dfcf68cd1723e36f
Victor Lucas Costa Viana <victor.viana@katrium.local>
2025-10-22 23:21:53 -0300

    feat: retorno via patente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './auth.guard';
 
-
-
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorPageComponent } from './error-page.component';
-
-
 
 export const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
